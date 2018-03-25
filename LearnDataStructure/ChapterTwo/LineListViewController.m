d9afdd94b51621ab004fb1098651ff7fef174ddc
= <=>
2018-03-25 23:12:07 +0800

    完成测试

--- LearnDataStructure/ChapterTwo/LineListViewController.m
+++ LearnDataStructure/ChapterTwo/LineListViewController.m
@@ -13,7 +13,6 @@
 #define MAXCOUNT 5
 #import "LineListViewController.h"
 
-
 @interface LineListViewController ()
 
 @end
@@ -22,6 +21,7 @@
 
 
 #pragma mark- LifeCicle
+
 - (void)viewDidLoad {
     [super viewDidLoad];
 }
@@ -29,24 +29,30 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
 }
-//0x7ffee9533e20
-//0x7f8336f61000
+
 - (void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
     // 测试线性表
-    List _list = initLineList();
-    for (int i = 0 ; i < MAXCOUNT ; i++ ) {
-        _list.data[i] = 1;
-    }
-    
-//    _list.data[MAXCOUNT] = {10.0f,0.0f,0.0f,0.0f,0.0f};
-//    List_point p = &(_list);
-    insertAtLast(&_list, 2.3);
-    insertAtLast(&_list, 32.3);
-    enumAllItems(&_list);
-    free(&(_list));
-//    free((struct Ste_List*)(*m));
-//    printf("%p\n",p);
+    ste_list_pointer list_pointer = initLineList();
+//    for (int i = 0 ; i < MAXCOUNT ; i++ ) {
+//        list_pointer->data[i] = 1;
+//    }
+// _list.data[MAXCOUNT] = {10.0f,0.0f,0.0f,0.0f,0.0f};
+    //insertAtLast(list_pointer, 1);
+    insertAtLast(list_pointer, 2);
+    //deleteByIndex(list_pointer, 0);
+    insertAtLast(list_pointer, 3);
+    insertAtLast(list_pointer, 4);
+    insertAtIndex(list_pointer, 5, 1);
+    insertAtIndex(list_pointer, 51,0);
+//    insertAtLast(list_pointer, 132);
+//    insertAtLast(list_pointer, 332);
+//    deleteByValue(list_pointer, 2);
+//    insertAtLast(list_pointer, 332);
+//    deleteByValue(list_pointer, 32);
+    enumAllItems(list_pointer);
+    printf("find index;%d",findIndex(list_pointer, 5));
+    free(list_pointer);
     
 }
 
@@ -77,20 +83,18 @@ struct List {
 };
  */
 
-struct Ste_List prtL; //定义一个结构体;
-typedef struct Ste_List List; //定义创建“结构体”的快捷方式;
-typedef struct Ste_List *List_point; //创建指向结构体指针的快捷方式;
+//struct Ste_List liner_struct; //定义一个结构体;
+typedef struct Ste_List ste_list; //定义创建“结构体”的快捷方式;
+typedef struct Ste_List *ste_list_pointer; //创建指向结构体指针的快捷方式;
 
 #pragma mark- 线性表初始化
 
-List initLineList() {
-    List_point ste_list;
-    ste_list = malloc(sizeof(List));
+ste_list_pointer  initLineList() {
+    ste_list_pointer ste_list;
+    ste_list = malloc(sizeof(ste_list));
 //    ste_list->data = {0.0,0.0,0.0,0.0,0.0};
     ste_list->last = -1; //初始化为-1
-    //printf("malloc address :%p\n",ste_list);
-    return *ste_list;
-   
+    return ste_list;
     /*
     另一种形式的初始化方法
     List* my_list;
@@ -113,7 +117,7 @@ List_point initLineList() {
 
 #pragma mark- 给定一个Value查找在数组中的位置
 // 给定一个值找出来,这个值所在的位置
-int findIndex(List_point list,float value) {
+int findIndex(ste_list_pointer list,int value) {
     int i = 0;
     while (0 <= list->last && list->data[i] != value) {
         i++;
@@ -128,7 +132,7 @@ int findIndex(List_point list,float value) {
 
 #pragma mark- 结尾插入操作
 // 在结尾插入
-bool insertAtLast(List_point list,float value) {
+bool insertAtLast(ste_list_pointer list,int value) {
     if(list->last + 1 < MAXCOUNT) {
         list->last++;
         list->data[list->last] = value;
@@ -139,7 +143,7 @@ bool insertAtLast(List_point list,float value) {
 }
 #pragma mark- 中间插入操作
 // 约束条件 0 <= index <= list->last && MAXCOUNT - list->last > 1 表没有满
-bool insertAtIndex(List_point list,float value,int index) {
+bool insertAtIndex(ste_list_pointer list,int value,int index) {
     if(index >= 0 && MAXCOUNT - list->last > 1 && index <= list->last) {
         for (int i = list->last; i >= index; i--) {
             //注意移动的顺序从最后往前移动
@@ -156,25 +160,34 @@ bool insertAtIndex(List_point list,float value,int index) {
 
 #pragma mark- 删除操作--根据Value
 // 数组中可能存在多个相同的值
-void deleteByValue(List_point list,float value) {
+void deleteByValue(ste_list_pointer list,int value) {
+    int deleteValueCounter = 0;
+    int deleteValueIndexArray[MAXCOUNT] = {0};
     for (int i = 0; i <= list->last; i++) {
         if (list->data[i] == value) {
-            for (int j = i; j < list->last; j++) {
-                list->data[j] = list->data[j+1];
-            }
+            list->data[i] = 0;
+            deleteValueIndexArray[deleteValueCounter] = i - deleteValueCounter; //每添加一个下标，操作步骤少deleteValueCounter
+            deleteValueCounter++;
         }
     }
+    for (int index = 0 ; index < deleteValueCounter ; index++) {
+        for (int j = deleteValueIndexArray[index]; j < list->last; j++) {
+            list->data[j] = list->data[j+1];
+            list->data[j+1] = 0; //清零
+        }
+    }
+    list->last -= deleteValueCounter; //更新指针;
     //时间复杂度O(n的平方)
     // 可以先找到Value对应的index, 然后根据各个index进行相应的删除操作;
 }
 
 #pragma mark- 删除操作--根据Index
-bool deleteByIndex(List_point list,int index) {
+bool deleteByIndex(ste_list_pointer list,int index) {
     if (list->last >= index && index >=0 ) {
         for (int i = index; i < list->last; i++) {
             list->data[i] = list->data[i+1];
         }
-        list->data[index] = 0.0f;
+        list->data[list->last] = 0;
         list->last--;
         return true;
     }else{
@@ -183,13 +196,10 @@ bool deleteByIndex(List_point list,int index) {
 }
 
 #pragma mark- 遍历所有元素
-void enumAllItems(List_point list) {
+void enumAllItems(ste_list_pointer list) {
     for (int i = 0 ; i <= list->last; i++) {
-        printf("value: %.2f",list->data[i]);
+        printf("value: %d\n",list->data[i]);
     }
 }
 
-- (NSString*)description {
-    return @"This is Custom method!";
-}
 @end
